8606868eb8564c8c870bf4db8d2c31caebe51156
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-03 22:27:47 +0800

    log login email and password

--- Client/app/src/main/java/edu/cuhk/csci3310/cupo/LoginActivity.java
+++ Client/app/src/main/java/edu/cuhk/csci3310/cupo/LoginActivity.java
@@ -3,6 +3,10 @@ package edu.cuhk.csci3310.cupo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText loginEmailInput = findViewById(R.id.loginEmailInput);
+        EditText loginPasswordInput = findViewById(R.id.loginPasswordInput);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("cupo", loginEmailInput.getText().toString() + " " + loginPasswordInput.getText().toString());
+            }
+        });
     }
 }
