c85a6c0e4ddd41b771f371a2029fc868af8e6e1e
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-05 00:49:14 +0800

    use timestamp as part of password salt

--- Server/utils.py
+++ Server/utils.py
@@ -1,8 +1,8 @@
 import hashlib
 
 
-def passwordHash(password, userId, salt='fd975e94f51920b4'):
-    salt += str(userId)
+def passwordHash(password, ts, salt='fd975e94f51920b4'):
+    salt += str(ts)
     saltedPassword = password + salt
     # calculate hash(saltedPassword)
     return hashlib.sha256(saltedPassword.encode()).hexdigest()
