1451fffd6249687ed486c52f226d2b0553a4fc6e
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-05 00:35:35 +0800

    create user with timestamp

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,7 @@
 import sql
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +18,8 @@ def root():
 @app.route('/create_user', methods=['POST'])
 def create_user():
     req = request.json
-    if sql.createUser(req['email'], req['email'], req['password']):
+    ts = int(time.time() * 1000)
+    if sql.createUser(req['email'], req['email'], req['password'], ts):
         return '', 201
     else:
         return '', 409
