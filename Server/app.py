d97035fc1279ca6f1c7b69a83d1daa7e2d09b23e
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-04 20:00:27 +0800

    /create_user

--- Server/app.py
+++ Server/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+import sql
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def root():
-    return 'CUPO'
+    return jsonify({
+        'POST': '/create_user'
+    })
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    req = request.json
+    sql.createUser(req['email'], req['email'], req['password'])
+    return '', 201
