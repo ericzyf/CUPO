f90c62908b3cbe80c10cee0e620e56a951d70260
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-05 13:58:09 +0800

    store global params in SharedPreferences

--- Client/app/src/main/java/edu/cuhk/csci3310/cupo/MainActivity.java
+++ Client/app/src/main/java/edu/cuhk/csci3310/cupo/MainActivity.java
@@ -2,15 +2,26 @@ package edu.cuhk.csci3310.cupo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "edu.cuhk.csci3310.cupo.pref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set up SharedPreferences
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        prefs.edit()
+                .putString("API_URL", "http://118.195.184.108:5000")
+                .apply();
+
         startActivity(new Intent(this, LoginActivity.class));
     }
 }
