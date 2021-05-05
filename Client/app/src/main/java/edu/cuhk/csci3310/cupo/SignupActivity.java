73969d227653e53d1130c7960a3065b983e50fd4
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-05 11:59:46 +0800

    Signup get text

--- Client/app/src/main/java/edu/cuhk/csci3310/cupo/SignupActivity.java
+++ Client/app/src/main/java/edu/cuhk/csci3310/cupo/SignupActivity.java
@@ -3,6 +3,10 @@ package edu.cuhk.csci3310.cupo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -12,5 +16,16 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         getSupportActionBar().setTitle("Sign Up");
+
+        EditText signupEmailInput = findViewById(R.id.signupEmailInput);
+        EditText signupPasswordInput = findViewById(R.id.signupPasswordInput);
+        EditText signupPasswordInput2 = findViewById(R.id.signupPasswordInput2);
+        Button signup = findViewById(R.id.signupButton);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("cupo", signupEmailInput.getText().toString() + "," + signupPasswordInput.getText().toString() + "," + signupPasswordInput2.getText().toString());
+            }
+        });
     }
 }
