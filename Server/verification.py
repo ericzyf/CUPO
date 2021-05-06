111f1285f6f4a6f8b0e273e8b241c371fba14f29
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-06 20:19:36 +0800

    sendCode

--- Server/verification.py
+++ Server/verification.py
@@ -1,8 +1,12 @@
 import random
+import redis
 import smtplib
 import string
 from dotenv import dotenv_values
 from email.message import EmailMessage
+
+R = redis.Redis(host='localhost', port=6379, db=0)
+CODE_TTL = 300  # each verification code expires after 5 mins
 
 
 # Return a random string consists of uppercase letters and digits of length 6
@@ -27,4 +31,23 @@ def sendMail(recv, code):
     s.login(SENDER, PWD)
     s.send_message(msg)
     s.quit()
+
+
+def sendCode(recv):
+    # check redis if there is existing unexpired code
+    res = R.get(recv)
+    if res is not None:
+        # get previous code
+        prevCode = res.decode()
+        # renew previous code
+        R.expire(recv, CODE_TTL)
+        # send again
+        sendMail(recv, prevCode)
+    else:
+        # create new code
+        newCode = genCode()
+        # store in redis
+        R.set(recv, newCode, ex=CODE_TTL)
+        # send
+        sendMail(recv, newCode)
 
