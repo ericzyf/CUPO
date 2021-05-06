4102071a3418747984217a09cd78c9ee397516df
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-06 13:27:34 +0800

    utils.isCUHKEmail

--- Server/utils.py
+++ Server/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import re
 
 
 def passwordHash(password, ts, salt='fd975e94f51920b4'):
@@ -6,4 +7,10 @@ def passwordHash(password, ts, salt='fd975e94f51920b4'):
     saltedPassword = password + salt
     # calculate hash(saltedPassword)
     return hashlib.sha256(saltedPassword.encode()).hexdigest()
+
+
+cuhk_email_regex = re.compile(r"^\w+@(link.)?cuhk.edu.hk$", flags=re.ASCII)
+
+def isCUHKEmail(email):
+    return cuhk_email_regex.match(email)
 
