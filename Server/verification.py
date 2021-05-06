359aa773a354d77b0f612a0fbc5b608b5731002f
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-06 13:59:45 +0800

    .

--- Server/verification.py
+++ Server/verification.py
@@ -2,6 +2,7 @@ import random
 import string
 
 
+# Return a random string consists of uppercase letters and digits of length 6
 def genVerificationCode():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
