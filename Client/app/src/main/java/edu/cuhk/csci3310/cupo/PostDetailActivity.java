76bca027174718603069f94eae631b7974afcc98
Yifan Zhang <yfzhang1999@gmail.com>
2021-05-14 05:47:33 +0800

    PostDetailActivity layout

--- Client/app/src/main/java/edu/cuhk/csci3310/cupo/PostDetailActivity.java
+++ Client/app/src/main/java/edu/cuhk/csci3310/cupo/PostDetailActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Timestamp;
 
 public class PostDetailActivity extends AppCompatActivity {
 
     private Backend.Post post;
+    private TextView postDetailTitle;
+    private TextView postDetailUsername;
+    private TextView postDetailTime;
+    private TextView postDetailContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,22 @@ public class PostDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         post = intent.getParcelableExtra("post");
+
+        postDetailTitle = findViewById(R.id.postDetailTitle);
+        postDetailUsername = findViewById(R.id.postDetailUsername);
+        postDetailTime = findViewById(R.id.postDetailTime);
+        postDetailContent = findViewById(R.id.postDetailContent);
+
+        postDetailTitle.setText(post.getTitle());
+        postDetailUsername.setText("@" + post.getUsername());
+        postDetailTime.setText(new Timestamp(post.getTimestamp()).toString());
+        postDetailContent.setText(post.getContent());
+
+        postDetailUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
     }
 }
